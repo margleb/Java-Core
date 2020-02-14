40c7c98f3f355bbd6e7510863360f4814d61d4e9
margleb <margleb@yandex.ru>
2020-02-14 14:12:17 +0300

    Урок по Java 6  Типы данных.

--- src/Human.java
+++ src/Human.java
@@ -1,22 +1,27 @@
 public class Human {
-    int age;
-    double weight;
-    public static void main(String[] args)
-    {
-        Human human = new Human();
-        human.buyBread();
-        human.buyMilk();
-        System.out.println("Hello World!");
-    }
-    void birthday() {
-        age = age + 1;
-    }
+    // 8 примитивныx типов данных в JAVA
 
-    void buyBread() {
-        System.out.println("buy bread");
-    }
+    // целочичленные
+    byte b = 0; // 1 байт(256) -128 до 128
+    short s = 1234; // 2 байт -32 до 32 тыс
+    int i = 24242142; // 4 байт(4)
+    long l = 1232132; // 8 байт
+    // c плавающей точкой
+    float f = 12.234f; // 4, f - указатель float
+    double d = 1284.32314;
+    // логический
+    boolean bool = true;
+    // один cимвол
+    char cr = 'd';
 
-    void buyMilk() {
-        System.out.println("buy milk");
+    // ссылочные типы
+    Human friend = new Human();
+    Human mom;
+    Human dad;
+
+    Car car = new Car();
+
+    public static void main(String[] args) {
+
     }
 }
