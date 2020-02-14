fb7abe0a4ea39df393c5bbcad4d6841150d42b47
margleb <margleb@yandex.ru>
2020-02-14 14:59:17 +0300

    Урок по Java 9  Пакеты и импорты(Packages and Import)

--- src/Human.java
+++ src/Human.java
@@ -1,22 +1,14 @@
-/*
- Существует 3 вида распределения памяти в JAVA
- - Java Heap - используется для выделения памяти обьектам и классам JRE
- - Stack Memory - используется для выполнения потока (thread)
- - PermGen - содержит метаданные классов и объектов
- */
+// импорт класса
+import com.max.*;
+// класс test виден только на уровне пакета com.max
+// import com.max.Test;
 
 public class Human {
-    int age = 20;
+    // com.max.Сar car;
+    Сar dadCar;
+    Plane plane;
+
     public static void main(String[] args) {
-        Human human = new Human();
-        System.out.println(human.getAge(5, 0.2,true, human));
-    }
-    // примитивы при передаче в метод хранятся в Stack, а при передачи
-    // обьектов - передается ссылка на него, сам обьект храниятся в Heap
-    double getAge(int i, double d, boolean bool, Human human) {
-        double newAge = i + d;
-        return human.age = (int) newAge;
-        // цикл забивает память Stack (StackOverflowError)
-        // return human.getAge(i, d, bool, human);
+
     }
 }
---
+++ src/com/max/Plane.java
@@ -0,0 +1,6 @@
+package com.max;
+
+public class Plane {
+  // импортировать нет смысла, так как находимся в одном классе
+  Test test;
+}
