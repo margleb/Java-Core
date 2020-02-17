9512b63a7aa017e5276b523881d1329118251526
margleb <margleb@yandex.ru>
2020-02-17 12:53:40 +0300

    Урок по Java 39 Циклы while и do while

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,21 @@ public class Main extends Object {
     enum Days { MONDAY, THUSDAY, WENSDAY }
 
     public static void main(String[] args) {
-        // можно передать примитивы, строки или enum
-        // без оператора break выведутся другие кейсы после условия
-        // дефолт не обязательно должен быть последним
-        int a = 1;
-        String str = "string";
-        Days thusday = Days.THUSDAY;
-        switch(thusday) {
-            case MONDAY:
-                System.out.println(Days.MONDAY);
-                // break;
-            case THUSDAY:
-                System.out.println(Days.THUSDAY);
-                // break;
-            case WENSDAY:
-                System.out.println(Days.WENSDAY);
-                // break;
-            default:
-                System.out.println("No idea!");
-                // break;
-        }
+       int [] arr = {1,2,3};
+       int i = 0;
+       while(i == 5) {
+           System.out.println(i);
+           i++;
+       }
+
+       System.out.println();
+
+       i = 0;
+       // do выполнится как минимум один раз
+       do {
+           System.out.println(arr[i]);
+           i++;
+       } while(false);
+
     }
 }
