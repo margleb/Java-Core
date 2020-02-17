713965a31a6aacf8797cb8e8126bf3d7ad7da25a
margleb <margleb@yandex.ru>
2020-02-17 13:13:02 +0300

    Урок по Java 41 Улученный цикл For

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main extends Object {
 
     public static void main(String[] args) {
         int[] arr = {1,2,3};
-        int i = 0;
-        for(System.out.println("This is first statement!"); i < arr.length; System.out.println("end"), i++)
-            System.out.println(arr[i]);
+        for(int i: arr) {
+            System.out.println(i);
+        }
     }
 }
