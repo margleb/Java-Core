45a057371cd9f0b7df24ed4a8833ef35ecd1dbd6
margleb <margleb@yandex.ru>
2020-02-18 10:50:34 +0300

    Урок по Java 44 StringBuilder

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-// В командной строке пишем:
-// 1. В компиляторе прописываем - javac -ea (либо enableassesion) Main
-// 2. Запускаем лаунчер java -ea Main
-// В настройках IntelliJ IDEA в VM Options прописываем -ea
+/*
+ stringBuilder - если требуется постоянное изменение строк, то лучше использовать StringBuilder
+ stringBuffer -  Потокобезопасная, изменяемая последовательность символов
+ */
 
 public class Main {
     public static void main(String[] args) {
-    new Main().someMethod(-5);
-    }
-    private void someMethod(int a) {
-        // ошибка assession error
-        assert (a > 0);
-        System.out.println(a);
+         String s1 = new String("abc");
+         String s2 = s1.concat("def");
+         // строка s не изменяемая
+         // System.out.println(s1);
+         // System.out.println(s2);
+
+         // StringBuilder
+        StringBuilder stringBuilder = new StringBuilder("abc");
+        // Добавляет указанный строку к этой последовательности
+        stringBuilder.append("def");
+        // Вставляет строку в эту последовательность символов.
+        stringBuilder.insert(3, "-");
+
+
+        // все методы синхронизированы, используется для многопоточных приложений
+        // StringBuffer stringBuffer;
+
     }
 }
