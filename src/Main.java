312a138db4e389aadfe44febad6abbfd53cf6222
margleb <margleb@yandex.ru>
2020-02-22 10:36:19 +0300

    Поразрядный сдвиг вправо и влево (самостоятельно изучил)

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,34 @@
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
-
-class Person implements Comparable<Person> {
-    private int age;
-
-    Person(int age) {
-        this.age = age;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public int compareTo(Person obj) {
-        return this.age - obj.age;
-    }
-
-    @Override
-    public String toString() {
-        return getClass().getName() + "{age = " + age + "}";
-    }
-
-}
-
-// В случае, если мы не можем импламентировать интерфейс Comparable
-// Например, если у нас класс Person недоступен, т.к. он является бибилиотекой
-// То мы можем использовать интерфейс Comparator
-class СomparePerson implements Comparator<Person> {
-    @Override
-    public int compare(Person p1, Person p2) {
-        return p1.getAge() - p2.getAge();
-    };
-}
-
-
 public class Main {
     public static void main(String[] args) {
-        // ComparableInterface();
-        ComaparatorInterface();
+        //  0 0 0 0 1 1 0 1
+        int a = 13; // 1101
 
-    }
+        /** Позиция при сдвиге влево **/
+        // при сдвиге, последние биты заполняем нулями
+        // 13 * 2
+        // 0 0 0 1 1 0 1 0
+        int b = a << 1;
+        // System.out.println(b); // 11010 (26)
 
-    public static void ComparableInterface() {
-        // Уже импламентирует интерфейс Comparable
-        // Integer integer;
-        Set set = new TreeSet();
-        set.add(new Person(4));
-        set.add(new Person(6));
-        set.add(new Person(4));
-        set.add(new Person(2));
-        set.add(new Person(3));
-        for(Object obj: set) {
-            System.out.println(obj);
-        }
-    }
+        // 0 0 1 1 0 1 0 0
+        // 13 * 2²
+        int с = a << 2;
+        // System.out.println(с); // 110100 (52)
 
-    public static void ComaparatorInterface() {
-        СomparePerson comparePerson = new СomparePerson();
-        Set set = new TreeSet(comparePerson);
-        set.add(new Person(4));
-        set.add(new Person(6));
-        set.add(new Person(4));
-        set.add(new Person(2));
-        set.add(new Person(3));
-        for(Object obj: set) {
-            System.out.println(obj);
-        }
+        //  0 1 1 0 1 0 0 0
+        // 13 * 2³
+        int d = a << 3;
+        // System.out.println(d); // 1101000 (78)
+
+        /** Позиция при сдвиге вправо **/
+        //  0 0 0 0 0 1 1 0
+        // 13 / 2
+        int e = a >> 1;
+        System.out.println(e); // 110 (6)
+
+        //  0 0 0 0 0 0 1 1
+        // 13 / 2²
+        int r = a >> 2;
+        System.out.println(r); // 011 (3)
     }
-};
+}
