40dff2a8b117466cc16c7e29eb720f7a96760d5f
margleb <margleb@yandex.ru>
2020-02-23 17:33:48 +0300

    Урок по Java 59: Клонирование объектов

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,47 @@
-class Popcorn {
-    void doSomething() {
-        System.out.println("popcorn");
-    }
-    void secondMethod() {
-        System.out.println("second");
-    }
-};
+/*
+Cуществует два вида клонирования:
+1. Поверхностное - копирует примитивные типы данных
+2. Глубокое - требует копирование всех ссылочных обьектов
+
+ */
 
 public class Main {
-    public static void main(String[] args) {
-        // #1
-        // создается анонимный класс, расширяемый Popcorn()
-        Popcorn popcorn = new Popcorn() {
-            void doSomething() {
-                System.out.println("main");
-            }
-        };
-        popcorn.doSomething();
-        popcorn.secondMethod();
+    public static void main(String[] args) throws Exception {
+        MyObject1 myObject1 = new MyObject1();
+        myObject1.a = 1;
+        MyObject2 myObject2 = new MyObject2();
+        myObject2.b = 3;
 
-        // #2
-        // С помочью анонимных классов можно переопределить метод Comparable
-        Comparable comparable = new Comparable() {
-            @Override
-            public int compareTo(Object o) {
-                return 0;
-            }
-        };
-        comparable.compareTo(new Object());
+        myObject1.LinkToMyObject2 = myObject2;
+        // MyObject1 copyMyObject1 = myObject1;
+        // создается новый обьект и копируется
+        MyObject1 copyMyObject1 = myObject1.clone();
 
-        // #3
-        // Анонимный класс в качестве аргумента
-        method(new Popcorn() {
-           void doSomething() {
-            System.out.println("Ананимный класс работает");
-           }
-        });
-
+        copyMyObject1.a = 2;
+        System.out.println(myObject1.a);
+        copyMyObject1.LinkToMyObject2.b = 4;
+        System.out.println(myObject1.LinkToMyObject2.b);
     }
-    static void method(Popcorn popcorn) {
-        popcorn.doSomething();
+}
+
+// указываем маркерный интерфейс Clonable
+class MyObject1 implements Cloneable {
+    int a;
+    MyObject2 LinkToMyObject2;
+
+    @Override
+    protected MyObject1 clone() throws CloneNotSupportedException {
+        // return (MyObject1) super.clone();
+        MyObject1 myObject1 = (MyObject1) super.clone();
+        myObject1.LinkToMyObject2 = LinkToMyObject2.clone();
+        return myObject1;
+    }
+}
+
+class MyObject2 implements Cloneable {
+    int b;
+    @Override
+    protected MyObject2 clone() throws CloneNotSupportedException {
+        return (MyObject2) super.clone();
     }
 }
