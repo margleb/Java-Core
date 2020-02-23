b678a5dccea6a0f1be55cd392e1f236ff17f6bc1
margleb <margleb@yandex.ru>
2020-02-23 14:06:48 +0300

    Урок по Java 57 Inner Classes - Внутренние классы

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,77 @@
 /*
-Обобщения или generics (обобщенные типы и методы) позволяют нам уйти от жесткого определения используемых типов.
-Generics применяются в о сновном в коллекциях, дабы избежать случайного добавления в список не того типа данных
-*/
+Существуют статические и внутренние классы
+Обычные (inner) классы имеют доступ до всех полей (даже приватных)
+Cтатические внутренние классы
+Локальные классы (внутренние классы на уровне методов)
+ */
 
 public class Main {
+
+    private int i = 5;
+    static int q = 5;
+
+    static class StaticClass {
+        static void methodStatic() {
+            // статические классы не имеют доступа до полей
+            // System.out.println(i);
+            System.out.println(q);
+        }
+    }
+
     public static void main(String[] args) {
-        Main main = new Main();
         // #1
-        // main.<String>GenericExampleMethodOne("123");
-        // main.GenericExampleMethodOne(123);
-        // main.GenericExampleMethodOne(new Main());
+        // Inner inner = new Inner();
+        Main main = new Main();
+        main.innerInit();
 
         // #2
-        // main.GenericExampleMethodTwo("123", 123, new Object());
+        // пример создания экз. внутреннего класса
+        Inner inner = main.new Inner();
+        inner.innerMethod();
+
+        // #3
+        StaticClass.methodStatic();
+
+        // #4
+        new Main().methodWithClass();
+
     }
 
-    // обобщенный метод, работает с любыми типами
-    <A> A GenericExampleMethodOne (A type) {
-        System.out.println(type);
-        return type;
+    void innerInit() {
+        // Не может быть вызван в статическом контексте
+        Inner inner = new Inner();
+        inner.innerMethod();
+        // локальная переменная класса Inner
+        System.out.println(inner.k);
     }
 
-    // обобщенный метод, c передачей нескольких типов
-    <B, C, D> void GenericExampleMethodTwo(B t, C u, D k) {
-        System.out.println(t);
-        System.out.println(u);
-        System.out.println(k);
+    void methodWithClass() {
+        // перед классом я не могу создать экземпляр
+        // я также не могу его возращать из метода
+        // NewInner newInner = new NewInner();
+        class NewInner {
+            void newMethod() {
+                System.out.println("Hi!");
+            }
+        }
+        NewInner newInner = new NewInner();
+        newInner.newMethod();
+    }
+
+    class Inner {
+        private int k = 6;
+        void innerMethod() {
+            // имеет доступ до до поля i (даже приватного)
+            System.out.println(i);
+            System.out.println(q);
+        }
+    }
+}
+
+class OuterClass {
+    void method() {
+        // #3 Пример создания из внешнего класса экз.класса inner
+        Main.Inner inner  = new Main().new Inner();
+        inner.innerMethod();
     }
 }
