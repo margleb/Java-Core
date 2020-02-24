9394183bc3700931df8f4dd9bccc825a8ced0018
margleb <margleb@yandex.ru>
2020-02-24 12:57:02 +0300

    Урок по Java 60 Reflection

--- src/reflection/MyClass.java
+++ src/reflection/MyClass.java
@@ -1,24 +1,15 @@
-/*
-Рефлексия (от позднелат. reflexio — обращение назад) — это механизм исследования данных о программе во время её выполнения.
-Рефлексия позволяет исследовать информацию о полях, методах и конструкторах классов.
-*/
 package reflection;
 
-    public class MyClass {
-        private int number;
-        // у поля не getter фунции
-        private String name = "default";
-        public int getNumber() {
-            return number;
-        }
-        public void setNumber(int number) {
-            this.number = number;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-        // метод ошибочно приватный
-        private void printData(){
-            System.out.println(number + name);
-        }
+public class MyClass {
+    private transient int i;
+    // String s;
+    public MyClass() {
     }
+    // public MyClass(String s) {
+        //this.s = s;
+    // }
+    public synchronized String someMethod(String s) {
+        System.out.println("this is " + s);
+        return s;
+    }
+}
