507744fe89d4b24d125cea4b1902e11d984198a5
margleb <margleb@yandex.ru>
2020-02-25 12:46:39 +0300

    Урок по Java 68 Многопоточность 3 Синхронизация

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,76 @@
 /*
-  Thread.sleep(1000);  - усы пляет ТЕКУЩИЙ ПОТОК поток на одну секунду
-  myThread.setPriority() - выставление приоритетов потоков, если хотим чтобы один из них выполнялся чаще остальных (не гарантировано)
-  Tread.yeild() - ТЕКУЩИЙ ПОТОК Уступает другому потоку. Планировщик может игнорировать данный процесс
-  myThread.join() - ожидает заврешение определенного потока, и лишь потом выполнить что либо в другом
-  Голодание потоков - когда какие то потоки выполняются чаще, чем остальные
-
-  У потоков есть несколько состояний:
-  1. new - инициализация потока
-  2. runnable - пул потоков
-  3. running - запущенный поток, в этом состоянии находится только один поток, остальные находятся в пуле. (могут переходить из одного сосотояния в другое)
-  4. waiting/blocked/sleeping - состояния блокировки, когда потоки можно приостановить на время
-  5. dead - окончание потока
-*/
+По аналогии с транспортным движением
+Thread.yeild() -  знак главная дорога (притормози, уступи дорогу тс на главной дороге)
+synchronized - светофор, пока процесс (автомобиль) не доедет до конца дороги, не влкючиться зеленый свет (работает с помочью локов, иногда называются мониторами)
+Thread.sleep() - на подобии пешеходного перехода, притормози и продолжи движение через определенное время
+myThread.join() - можно привести аналогию с пробкой, (пока не проедут одни, ты не сможешь продожлжить движение)
+myThread.setPriority() - установить приоритет движения для определенного потока
+Synchronized необходим тогда, когда несколько потоков используют один и тот же обьект
+ */
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
+        // создаем обьект resource, задаем изн. значение i
+        // synchronized работает с каким то одним обьектом, в данном случае resource
+        Resource resource = new Resource();
+        resource.setI(5);
+        // создаем 2 потока
         MyThread myThread = new MyThread();
+        myThread.setName("one");
+        MyThread myThread2 = new MyThread();
+
+        // заносим в них ресурсы
+        myThread.setResource(resource);
+        myThread2.setResource(resource);
+
+        // запускаем потоки
         myThread.start();
-        // Thread.sleep(1000); // усыпляет текущий поток на одну cек
-        // выставление приоритетов потоков, если хотим чтобы один из них выполнялся чаще остальных
-        // myThread.setPriority(Thread.MAX_PRIORITY);
-        // Thread.yield(); // текущий поток уступает другому потоку
-        myThread.join(); // ожидает заврешение определенного потока
-        System.out.println("thrad main");
+        myThread2.start();
+
+        // Ожидает когда данный поток завершится
+        myThread.join();
+        myThread2.join();
+
+        System.out.println(resource.getI());
     }
 }
 
 class MyThread extends Thread {
+    Resource resource;
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
     @Override
     public void run() {
-      for(int i = 0; i < 20; i++) {
-          try {
-              Thread.sleep(100);
-          } catch (InterruptedException e) {
-              e.printStackTrace();
-          }
-          System.out.println(i);
-      }
+        resource.changeI();
     }
 }
 
+class Resource {
+    private int i;
+
+    public int getI() {
+        return i;
+    }
+
+    // методы меняющие значение должны быть синхронизированы
+    public synchronized void setI(int i) {
+        this.i = i;
+    }
+
+    // synchronized - метод не должен прерываться (локи)
+    public synchronized void changeI() {
+        // можно синхронизировать какой то блок кода
+        synchronized (this) {
+            int i = this.i;
+            // Без synchronized может прерваться первый поток и начаться второй
+            if(Thread.currentThread().getName().equals("one")) {
+                // прекращается выполнение потока и он возращается в пулл потоков
+                // далее java, начинает выбирать какой либо поток из пулла
+                Thread.yield();
+            }
+            i++;
+            this.i = i;
+        }
+    }
+}
