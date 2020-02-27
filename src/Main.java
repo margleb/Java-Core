d6c85a54c5798588f16f2ee496d849ee17535a15
margleb <margleb@yandex.ru>
2020-02-27 12:34:00 +0300

    Урок по Java 73 Многопоточность 8 DeadLock

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,58 @@
-/*
-Concurrency это работа с многопоточным кодом.
-Обычно под этим понимают:
-1. Управление (создание/запуск/остановка) потоками виртуальной машины Java.
-2. Управление потоком выполнения программы (синхронизация потоков).
-3. Управление доступом к памяти (к данным) в многопоточной среде
-https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/package-summary.html
-
-
-Главное правило -- синхронизировать любой доступ к ресурсу (в данном случае коллекции), будь то чтение или запись. Как именно -- на первых порах не столько важно. Методы-обертки с модификатором synchronizhed -- как один из вариантов. Чем больше опыт работы с многопоточностью -- тем больше вариантов, в зависимости от требований.
-Также нужно помнить, что даже специализированные потокобезопасные коллекции (из java.util.concurrent.*) при определенных сценариях тоже могут нуждаться в дополнительной синхронизации.
-
-*/
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+/* Deadlock или дедлок или взаимная блокировка — это ошибка, которая происходит когда нити имеют циклическую зависимость от пары синхронизированных объектов. */
 
 public class Main {
-    //
-    List list = Collections.synchronizedList(new ArrayList<>());
-
     public static void main(String[] args) {
-        NameList nameList = new NameList();
-        nameList.add("first");
-        class MyThread extends Thread {
-            @Override
-            public void run() {
-                System.out.println(nameList.removeFirst());
-            }
-        }
-        MyThread myThread =  new MyThread();
-        myThread.setName("one");
-        myThread.start();
-        new MyThread().start();
-    }
+        ResourceOne resourceOne = new ResourceOne();
+        ResourceTwo resourceTwo = new ResourceTwo();
 
-    static class NameList {
-        List list = Collections.synchronizedList(new ArrayList<>());
-        public void add(String name) {
-            list.add(name);
-        }
-        // Даже используя synchronizedList, операции по работе с возвращенной коллекцией всё равно надо заворачивать в synchronized.
-        public synchronized String removeFirst() {
-            if(list.size() > 0) {
-                if(Thread.currentThread().getName().equals("one")) {
-                    Thread.yield();
-                }
-                // без синхронизации тут выйдет ошибка
-                return (String) list.remove(0);
-            }
-            return null;
-        }
+        resourceOne.resourceTwo = resourceTwo;
+        resourceTwo.resourceOne = resourceOne;
+
+        ThreadOne threadOne = new ThreadOne();
+        ThreadTwo threadTwo = new ThreadTwo();
+
+        threadOne.resourceTwo = resourceTwo;
+        threadTwo.resourceOne = resourceOne;
+
+        threadOne.start();
+        threadTwo.start();
+
+    }
+}
+
+class ThreadOne extends Thread {
+    ResourceTwo resourceTwo;
+    @Override
+    public void run() {
+        System.out.println(resourceTwo.getValue());
+    }
+}
+
+class ThreadTwo extends Thread  {
+    ResourceOne resourceOne;
+    @Override
+    public void run() {
+        System.out.println(resourceOne.getValue());
+    }
+}
+
+class ResourceOne {
+    ResourceTwo resourceTwo;
+    public synchronized int getValue() {
+        return resourceTwo.getInt();
+    }
+    int getInt() {
+        return 1;
+    }
+}
+
+class ResourceTwo {
+    ResourceOne resourceOne;
+
+    public synchronized int getValue() {
+        return resourceOne.getInt();
+    }
+    int getInt() {
+        return 2;
     }
 }
