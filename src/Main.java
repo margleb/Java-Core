2de83f2ba058df89b97e9b09b30491465ab011c4
margleb <margleb@yandex.ru>
2020-02-28 18:48:57 +0300

    Урок по Java 84 Многопоточность 19 Exchanger

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,47 @@
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Exchanger;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(3);
-        new Work(countDownLatch);
-        new Work(countDownLatch);
-        new Work(countDownLatch);
-        // ожидает окончания выполнения
-        countDownLatch.await();
-        System.out.println("all job done!");
+        Exchanger<String> exchanger = new Exchanger<>();
+        new Mike(exchanger);
+        new Anket(exchanger);
     }
-}
-
-class Work extends Thread {
-    CountDownLatch countDownLatch;
-
-    public Work(CountDownLatch countDownLatch) {
-        this.countDownLatch = countDownLatch;
-        start(); // запускает поток
-    }
-
-    @Override
-    public void run() {
-        try {
-            sleep(3000);
-        } catch(InterruptedException e) {
-            e.printStackTrace();
+    static class Mike extends Thread {
+      Exchanger<String> exchanger;
+      public Mike(Exchanger<String> excanger) {
+          this.exchanger = excanger;
+          start();
+      }
+        @Override
+        public void run() {
+            try {
+                exchanger.exchange("I'm 20 years old");
+                sleep(3000);
+                exchanger.exchange("Hi my name is Mike");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        System.out.println("done work");
-        countDownLatch.countDown();
     }
+
+    static class Anket extends Thread {
+        Exchanger<String> exchanger;
+        public Anket(Exchanger<String> exchanger) {
+            this.exchanger = exchanger;
+            start();
+        }
+        @Override
+        public void run() {
+            try {
+                // Выводится информация из другого потока
+                System.out.println(exchanger.exchange(null));
+                System.out.println(exchanger.exchange(null));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
