588e7eb78eb95f35b12ff4a808ce9b8dced0c46c
margleb <margleb@yandex.ru>
2020-02-28 17:44:18 +0300

    Урок по Java 82 Многопоточность 17 семофоры

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,51 @@
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Semaphore;
 
 public class Main {
+
     public static void main(String[] args) {
-        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-        scheduledExecutorService.schedule(new MyThread(), 3, TimeUnit.SECONDS);
-        scheduledExecutorService.shutdown();
+        // пропускная способность 2
+        Semaphore table = new Semaphore(3);
+
+        Person person = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+        Person person4 = new Person();
+        Person person5 = new Person();
+        Person person6 = new Person();
+        Person person7 = new Person();
+
+        person.table = table;
+        person2.table = table;
+        person3.table = table;
+        person4.table = table;
+        person5.table = table;
+        person6.table = table;
+        person7.table = table;
+
+        person.start();
+        person2.start();
+        person3.start();
+        person4.start();
+        person5.start();
+        person6.start();
+        person7.start();
+
     }
-    static class MyThread extends Thread {
-        @Override
-        public void run() {
-            System.out.println(1);
+}
+
+class Person extends Thread {
+    Semaphore table;
+    @Override
+    public void run() {
+        System.out.println(this.getName() + " waiting for table");
+        try {
+            table.acquire(); // занял столик
+            System.out.println(this.getName() + " eat at the table");
+            this.sleep(5000);
+            System.out.println(this.getName() + " release table");
+            table.release(); // освободил столик
+        } catch(InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
