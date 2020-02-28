6d5eb469e02fa53c5ddd1235522ef6eccd23670b
margleb <margleb@yandex.ru>
2020-02-28 17:09:29 +0300

    Урок по Java 81 Многопоточность 16 scheduledExecutorService

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,17 @@
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-/*
-*  Если в программе создается большое количество кратковременных потоков,тоимеет смысл использовать пулп отоков
-*  Другая причина для использования пула потоков — необходимость ограничить количество параллельно выполняющихся потоков
-*/
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        // Executors executor; // класс для создания пула потоков
-        // Executors.newSingleThreadExecutor(); // Пул с единственным потоком, выполняющим переданные ему задачи поочередно
-        // Executors.newCachedThreadPool(); // Новые потоки создаются по мере необходимости, а проста¬ ивающие потокисохраняютсявтечение 60 секунд
-
-        // Пул содержит фиксированное множество наборов потоков,а простаивающие потоки сохраняются в течение 60 секунд
-        ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(2);
-
-        // В результате вызова метода submit() возвращается объект типа Future, позволяющий оценить состояние задачи
-        newFixedThreadPool.submit(new MyRunnable()); // перадача задачи обьекту типа ExecutorService c помочью метода submit
-        System.out.println(newFixedThreadPool.submit(new MyCallable()).get()); // вернет Feture и вызовет метод get()
-
-        // Останавливает службу, завершая все запущенные задачи ине принимая новых.
-        newFixedThreadPool.shutdown();
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.schedule(new MyThread(), 3, TimeUnit.SECONDS);
+        scheduledExecutorService.shutdown();
     }
-
-    static class MyRunnable implements Runnable {
+    static class MyThread extends Thread {
         @Override
         public void run() {
             System.out.println(1);
-        }
-    }
-
-    static class MyCallable implements Callable<String> {
-        @Override
-        public String call() throws Exception {
-            return "2";
         }
     }
 }
