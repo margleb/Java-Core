c127545d4ac1639a1fc2ca0e0bb87a977488e53f
margleb <margleb@yandex.ru>
2020-02-28 18:36:45 +0300

    Урок по Java 83 Многопоточность 18 СountDownLatch

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,33 @@
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.CountDownLatch;
 
 public class Main {
-
-    public static void main(String[] args) {
-        // пропускная способность 2
-        Semaphore table = new Semaphore(3);
-
-        Person person = new Person();
-        Person person2 = new Person();
-        Person person3 = new Person();
-        Person person4 = new Person();
-        Person person5 = new Person();
-        Person person6 = new Person();
-        Person person7 = new Person();
-
-        person.table = table;
-        person2.table = table;
-        person3.table = table;
-        person4.table = table;
-        person5.table = table;
-        person6.table = table;
-        person7.table = table;
-
-        person.start();
-        person2.start();
-        person3.start();
-        person4.start();
-        person5.start();
-        person6.start();
-        person7.start();
-
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        new Work(countDownLatch);
+        new Work(countDownLatch);
+        new Work(countDownLatch);
+        // ожидает окончания выполнения
+        countDownLatch.await();
+        System.out.println("all job done!");
     }
 }
 
-class Person extends Thread {
-    Semaphore table;
+class Work extends Thread {
+    CountDownLatch countDownLatch;
+
+    public Work(CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+        start(); // запускает поток
+    }
+
     @Override
     public void run() {
-        System.out.println(this.getName() + " waiting for table");
         try {
-            table.acquire(); // занял столик
-            System.out.println(this.getName() + " eat at the table");
-            this.sleep(5000);
-            System.out.println(this.getName() + " release table");
-            table.release(); // освободил столик
+            sleep(3000);
         } catch(InterruptedException e) {
             e.printStackTrace();
         }
+        System.out.println("done work");
+        countDownLatch.countDown();
     }
 }
