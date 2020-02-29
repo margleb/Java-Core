df395ffae70563196f9dd4a96637fd2447f69cf2
margleb <margleb@yandex.ru>
2020-02-29 18:17:48 +0300

    Урок по Java 90 Многопоточность 25 ThreadLocalRandom and TimeUnit

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,12 @@
-/*
-ForkJoinPool - реализация ExecutorService , которая управляет рабочими потоками и предоставляет нам инструменты для получения информации о состоянии и производительности пула потоков.
-помогает ускорить параллельную обработку, пытаясь использовать все доступные ядра процессора
-*/
-
-
-import java.util.Date;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-
-    static long numOfOperations = 10_000_000_000L;
-    // Возвращает количество процессоров, доступных для виртуальной машины Java.
-    static int numOfThreads = Runtime.getRuntime().availableProcessors();
-
-    public static void main(String[] args) {
-        // System.out.println(new Date());
-        // System.out.println(numOfThreads);
-        // Cоздает экземпляр с указанным уровнем параллелизма
-        ForkJoinPool pool = new ForkJoinPool(numOfThreads);
-        // Выполняет заданное задание, возвращая его результат по завершении
-        System.out.println(pool.invoke(new MyFork(0, numOfOperations)));
-        // System.out.println(new Date());
-    }
-
-    // Рекурсивный результат
-    static class MyFork extends RecursiveTask<Long> {
-        long from, to;
-
-        public MyFork(long from, long to) {
-            this.from = from;
-            this.to = to;
-        }
-
-        @Override
-        // Основные вычесления выполняемые данной операцией
-        protected Long compute() {
-            // если операция разбита на достаточное кол-во частей
-            if ((to - from) <= numOfOperations / numOfThreads) {
-                // выполняем операцию
-                long j = 0;
-                for(long i = from; i < to; i++) {
-                    j+=i;
-                }
-                return j;
-            } else {
-                // разбиваем на части по меншьешу
-                long middle = (to + from) / 2;
-                MyFork firstHalf = new MyFork(from, middle);
-                firstHalf.fork();
-                MyFork secondHalf = new MyFork(middle + 1, to);
-                long secondValue = secondHalf.compute();
-                return firstHalf.join() + secondValue;
-            }
-        }
-    }
+        public static void main(String[] args) throws InterruptedException {
+            System.out.println(Math.random());
+            // для многопточных приложений используем данный класс
+            System.out.println(ThreadLocalRandom.current().nextInt());
+            // Количество миллисекунд в днях
+            System.out.println(TimeUnit.DAYS.toMillis(14));
+        };
 }
