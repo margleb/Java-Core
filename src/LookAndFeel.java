25fbd12018b7f11e7393eb50ba1847c1ef653abf
margleb <margleb@yandex.ru>
2020-03-02 18:04:27 +0300

    Урок по Java 101 Swing 6 WindowListener

--- src/LookAndFeel.java
+++ src/LookAndFeel.java
@@ -1,89 +1,18 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class LookAndFeel {
     public static void main(String[] args) {
-        // основные мотивы
-        // UIManager.LookAndFeelInfo[] lookAndFeelInfos = UIManager.getInstalledLookAndFeels();
-        // for(UIManager.LookAndFeelInfo lookAndFeelInfo : lookAndFeelInfos) {
-        // System.out.println(lookAndFeelInfo.getName());
-        // System.out.println(lookAndFeelInfo.getClassName());
-        // }
-
         JFrame jFrame = getFrame();
-        JPanel jPanel = new JPanel();
-        jFrame.add(jPanel);
-
-        JButton jButton1 = new JButton("Metal");
-        JButton jButton2 = new JButton("Nimbus");
-        JButton jButton3 = new JButton("CDE/Motif");
-        JButton jButton4 = new JButton("Windows");
-        JButton jButton5 = new JButton("Windows Classic");
-
-        jPanel.add(jButton1);
-        jPanel.add(jButton2);
-        jPanel.add(jButton3);
-        jPanel.add(jButton4);
-        jPanel.add(jButton5);
-
-
-        jButton1.addActionListener(new ActionListener() {
+        // Абстрактный класс адаптера для того, чтобы получить события окна. Методы в этом классе пусты. Этот класс существует как удобство для того, чтобы создать объекты слушателя
+        // WindowListener - Интерфейс слушателя для того, чтобы получить события окна
+        jFrame.addWindowListener(new WindowAdapter() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                int i = 0;
             }
         });
-
-        jButton2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-
-        jButton3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-
-        jButton4.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-
-        jButton5.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-
-
     }
 
     static JFrame getFrame() {
