c68e78729526e6372a1965126baa2274f4658d2e
margleb <margleb@yandex.ru>
2020-03-03 14:05:40 +0300

    Урок по Java 105 Swing 10 Текстовые поля и т.д.

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,36 @@
-/*
-У нас есть 2 основных интерфейса:
-1. MouseMotionListener
-2. MouseListener
-Которые имплементят множество разных методов для работы с мышью, все они присутсвуют в MouseAdapter и мы можем переписать те, которые нам нужны
-
-
- */
-
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 
 public class Main {
 
     static JFrame jframe = getFrame();
-
-    // cоздаем панель
-    // static JPanel jpanel = new JPanel();
+    static JPanel jpanel = new JPanel();
 
     public static void main(String[] args) {
+        jframe.add(jpanel);
 
-        // jframe.add(jpanel);
-        // jpanel.addMouseListener(new MouseAdapter() {
-            // @Override
-            // public void mouseClicked(MouseEvent e) {
-                // super.mouseClicked(e);
-                // jpanel.setBackground(Color.blue);
-            // }
-        // });
+        // Поле с видимостью в 20 символов
+        jpanel.add(new JTextField("default value",  20));
+        // JTextField jTextField = new JTextField();
+        // jTextField.getText();
 
-        // Отслеживание координат мыши
-        JComponent jComponent = new MyComponent();
-        jframe.add(jComponent);
-        jframe.addMouseMotionListener(new MouseAdapter() {
-            @Override
-            // при движении мыши будет происходить событие
-            public void mouseMoved(MouseEvent e) {
-                super.mouseMoved(e);
-                MyComponent.xCoord = e.getX();
-                MyComponent.yCoord = e.getY();
-                // repaint() перерисует координаты
-                jComponent.repaint();
-            }
-        });
-    }
+        // Добавляем лейбл к полю
+        jpanel.add(new JLabel("this is label"));
 
+        // Поле для паролей видимостью в 20 символов
+        jpanel.add(new JPasswordField(20));
 
-    static class MyComponent extends JComponent {
-        public static int xCoord;
-        public static int yCoord;
-        @Override
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            ((Graphics2D)g).drawString("Coorinates x: " + xCoord + " y " + yCoord, 50, 50);
-        }
+        // Текстовое поле
+        JTextArea JTextArea = new JTextArea(5, 20);
+        // Отменяем расширение в ширину
+        JTextArea.setLineWrap(true);
+
+        // Добавляем cкролл для текстового поля
+        JScrollPane jScrollPane = new JScrollPane(JTextArea);
+        jpanel.add(jScrollPane);
+
+        // Поддерживает отложенный автоматический макет
+        jpanel.revalidate();
     }
 
 
@@ -62,8 +38,9 @@ public class Main {
         JFrame jframe = new JFrame();
         jframe.setVisible(true);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.setBounds(20, 20, 300, 500);
+        jframe.setBounds(750, 250, 500, 500);
         jframe.setTitle("Example");
         return jframe;
     }
+
 }
