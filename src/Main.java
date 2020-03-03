87071fca8e5bfe1b4e9b32e20cf62fdcb63a0f23
margleb <margleb@yandex.ru>
2020-03-03 12:02:21 +0300

    Урок по Java 103 Swing 8 KeyStroke

--- src/Main.java
+++ src/Main.java
@@ -11,25 +11,36 @@ public class Main {
 
     public static void main(String[] args) {
         jframe.add(jpanel);
+        // абстрактный экшн
+        AbstractAction myAction = new MyAction();
         // создаем кнопку
-        JButton jbutton = new JButton(new MyAction());
+        JButton jbutton = new JButton(myAction);
         jbutton.setText("Кнопка");
         jpanel.add(jbutton);
+
+        // создаем сочетание клавиш
+        KeyStroke keyStroke = KeyStroke.getKeyStroke("ctrl B");
+        // создаем Map, c указанием при каком условии будет срабатывать сочетание клавиш (например, при фокусе)
+        InputMap inputMap = jpanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        // индентифицируем какой либо обьект и его событие
+        inputMap.put(keyStroke, "changeColor");
+        // при вызове определенного обьекта, вызывается action
+        ActionMap actionMap = jpanel.getActionMap();
+        actionMap.put("changeColor", myAction);
     }
 
-    // класс AbstractAction можно переиспользовать в других местах
     static class MyAction extends AbstractAction {
 
-        MyAction() {
-            // возможность создания экшенов (краткое описание)
-            putValue(AbstractAction.SHORT_DESCRIPTION, "My small action");
-        }
+        // MyAction() {
+            // putValue(AbstractAction.SHORT_DESCRIPTION, "This is gleb");
+        // }
 
         @Override
         public void actionPerformed(ActionEvent e) {
             jpanel.setBackground(Color.blue);
         }
     }
+
 
     static JFrame getFrame() {
         JFrame jframe = new JFrame();
