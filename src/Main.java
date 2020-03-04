8ef33d87ca99bd8f6302aa81ee6150993f37d2ec
margleb <margleb@yandex.ru>
2020-03-04 19:50:03 +0300

    Урок по Java 119 Swing 24 File Dialog

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
 
 
 public class Main {
@@ -11,23 +15,57 @@ public class Main {
 
     public static void main(String[] args) {
         jframe.add(jpanel);
-        JButton jButton = new JButton("Show Modal");
+        JButton jButton = new JButton("show file dialog");
         jpanel.add(jButton);
         jButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                new MyModal().setVisible(true);
+                JFileChooser fileChooser = new JFileChooser();
+                // setMultiSelectionEnabled - возможность выбора нескольких файлов
+                
+                fileChooser.setMultiSelectionEnabled(true);
+                // выбор по умолчанию (файлы и дериетории)
+                fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                // фильтр только gif файлы
+//                fileChooser.setFileFilter(new FileFilter() {
+//                    @Override
+//                    public boolean accept(File f) {
+//                        if(f.getName().endsWith("gif")) {
+//                            return true;
+//                        }
+//                        return false;
+//                    }
+//                    @Override
+//                    public String getDescription() {
+//                        return "only gif giles!";
+//                    }
+//                });
+
+                // добавляем текстовое поле
+                // TextField textField = new TextField("Раз два три");
+                // fileChooser.add(textField);
+
+//                fileChooser.addPropertyChangeListener(new PropertyChangeListener() {
+//                    @Override
+//                    public void propertyChange(PropertyChangeEvent evt) {
+//                        // при изменении файла в проводнике, меняется его название
+//                        textField.setText(evt.getNewValue().toString());
+//                    }
+//                });
+
+
+                // показать диалоговое окно
+                fileChooser.showDialog(jpanel, "save as");
+
+
+                File file = fileChooser.getSelectedFile();
+                // получить имя выбранного файла
+                System.out.println(file.getName());
+
             }
         });
-    }
 
-    static class MyModal extends JDialog {
-        public MyModal() {
-            super(jframe, "title", true);
-            add(new JTextField(), BorderLayout.NORTH);
-            add(new JButton("Кнопка"), BorderLayout.SOUTH);
-            setBounds(0, 0, 500,300);
-        }
+        jpanel.revalidate();
     }
 
     static JFrame getFrame() {
