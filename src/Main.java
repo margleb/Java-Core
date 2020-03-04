b91906337d8aa801fa40f139b1af279664294b00
margleb <margleb@yandex.ru>
2020-03-04 18:23:19 +0300

    Урок по Java 118 Swing 23 Custom dialog

--- src/Main.java
+++ src/Main.java
@@ -11,23 +11,26 @@ public class Main {
 
     public static void main(String[] args) {
         jframe.add(jpanel);
-        JButton jbutton = new JButton("show dialog");
-        jpanel.add(jbutton);
-        jbutton.addActionListener(new ActionListener() {
+        JButton jButton = new JButton("show dialog");
+        jpanel.add(jButton);
+        jButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // высплывающее окно со значком error
-                // JOptionPane.showMessageDialog(jpanel, "this is my dialog", "title", JOptionPane.ERROR_MESSAGE);
-                // окно conformation, возращает значение, с кастомные кнопками
-                //System.out.println(JOptionPane.showConfirmDialog(jpanel, "some massage", "title", JOptionPane.YES_OPTION));
-                // окно с выбором параметров
-                // JOptionPane.showOptionDialog(jpanel, "message", "title", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE,null, new Object[] {"one", "two", "three"}, "two" );
-                // поле для ввода
-                System.out.println(JOptionPane.showInputDialog(jpanel, "some message"));
+                MyDialog myDialog = new MyDialog();
+                myDialog.setVisible(true);
             }
         });
-    }
 
+    }
+    
+    static class MyDialog extends JDialog {
+        public MyDialog() {
+            super(jframe, "Title", true);
+            add(new JTextField(), BorderLayout.NORTH);
+            add(new JButton("button"), BorderLayout.SOUTH);
+            setBounds(500, 500, 250, 150);
+        }
+    }
 
     static JFrame getFrame() {
         JFrame jframe = new JFrame();
