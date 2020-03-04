9a9a33332f42100149e2c77d95e597b1343a5f2b
margleb <margleb@yandex.ru>
2020-03-04 15:32:17 +0300

    Урок по Java 116 Swing 21 Focus

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main {
 
     public static void main(String[] args) {
         jframe.add(jpanel);
-        jpanel.setLayout(null);
-
-        JButton jButton = new JButton("submit");
-        jButton.setBounds(50,50, 150, 40)
-        jpanel.add(jButton);
+        jpanel.add(new JButton("One"));
+        // у данного элемента фокуса не будет
+        jpanel.add(new JButton("Two")).setFocusable(false);
+        // в самом начале фокус будет на 3тьем элементе
+        jpanel.add(new JButton("Three")).requestFocusInWindow();
     }
 
 
