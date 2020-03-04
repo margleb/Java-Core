a8215bce21404dd5a7143d22c618a891bd7e25c4
margleb <margleb@yandex.ru>
2020-03-04 16:15:06 +0300

    Урок по Java 117 Dialogs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Main {
@@ -9,11 +11,21 @@ public class Main {
 
     public static void main(String[] args) {
         jframe.add(jpanel);
-        jpanel.add(new JButton("One"));
-        // у данного элемента фокуса не будет
-        jpanel.add(new JButton("Two")).setFocusable(false);
-        // в самом начале фокус будет на 3тьем элементе
-        jpanel.add(new JButton("Three")).requestFocusInWindow();
+        JButton jbutton = new JButton("show dialog");
+        jpanel.add(jbutton);
+        jbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // высплывающее окно со значком error
+                // JOptionPane.showMessageDialog(jpanel, "this is my dialog", "title", JOptionPane.ERROR_MESSAGE);
+                // окно conformation, возращает значение, с кастомные кнопками
+                //System.out.println(JOptionPane.showConfirmDialog(jpanel, "some massage", "title", JOptionPane.YES_OPTION));
+                // окно с выбором параметров
+                // JOptionPane.showOptionDialog(jpanel, "message", "title", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE,null, new Object[] {"one", "two", "three"}, "two" );
+                // поле для ввода
+                System.out.println(JOptionPane.showInputDialog(jpanel, "some message"));
+            }
+        });
     }
 
 
