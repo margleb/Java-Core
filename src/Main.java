2d2dd9b4b0d92d86bbd650c3de369185bd771b91
margleb <margleb@yandex.ru>
2020-03-04 14:10:51 +0300

    Урок по Java 113 Swing 18 GridBagLayout

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
+import java.awt.*;
 
-/* Для создания layout правой кнопкой мыши - контестное меню new -> Swing GUI -> UIForm */
 
 public class Main {
 
@@ -9,6 +9,55 @@ public class Main {
 
     public static void main(String[] args) {
         jframe.add(jpanel);
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        jpanel.setLayout(gridBagLayout);
+
+        GridBagConstraints constraints1 = new GridBagConstraints();
+        constraints1.weightx = 0;
+        constraints1.weighty = 0;
+        constraints1.gridx = 0;
+        constraints1.gridy = 0;
+        constraints1.gridheight = 2;
+        constraints1.gridwidth = 2;
+        jpanel.add(new JTextArea(4, 20), constraints1);
+
+        GridBagConstraints constraints2 = new GridBagConstraints();
+        constraints2.weightx = 0;
+        constraints2.weighty = 0;
+        // координаты (смещаемся на 2)
+        constraints2.gridx = 2;
+        constraints2.gridy = 0;
+        // занимаемые элементы по высоте/ширине
+        constraints2.gridheight = 1;
+        constraints2.gridwidth = 2;
+        jpanel.add(new JButton("submitVeryLong name"), constraints2);
+
+        GridBagConstraints constraints3 = new GridBagConstraints();
+        // ширина высота
+        constraints3.weightx = 0;
+        constraints3.weighty = 0;
+        // координаты (смещаемся на 2)
+        constraints3.gridx = 2;
+        constraints3.gridy = 1;
+        // занимаемые элементы по высоте/ширине
+        constraints3.gridheight = 1;
+        constraints3.gridwidth = 1;
+        jpanel.add(new JButton("submit11"), constraints3);
+
+        GridBagConstraints constraints4 = new GridBagConstraints();
+        // ширина высота
+        constraints4.weightx = 0;
+        constraints4.weighty = 0;
+        // координаты (смещаемся на 2)
+        constraints4.gridx = 3;
+        constraints4.gridy = 1;
+        // занимаемые элементы по высоте/ширине
+        constraints4.gridheight = 1;
+        constraints4.gridwidth = 1;
+        jpanel.add(new JButton("submit11"), constraints4);
+
+        jpanel.revalidate();
+
     }
 
 
