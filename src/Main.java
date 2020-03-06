4e0ec27d1c29e9b8385cb5c3b2b04c415aeb982d
margleb <margleb@yandex.ru>
2020-03-06 18:11:13 +0300

    Урок Java 129 AWT 9 Чтение и запись изображений

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,89 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.geom.Ellipse2D;
+/*
+BufferedImage - буфер данных изображения после прочтения
+ImageIO - основной класс для кодирования/декодирования прочтения и записи изображений
+*/
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-    static JFrame jframe = getFrame();
-    static JPanel jpanel = new JPanel();
+        // # 1 Чтение и запись изображений с помочью пакета ImageIO
 
-    public static void main(String[] args) {
-        jframe.add(jpanel);
-        jframe.add(new MyComponent());
-    }
-    static class MyComponent extends JComponent {
-        @Override
-        public void paint(Graphics g) {
-            Graphics2D gr2d = (Graphics2D) g;
+        // абстрактное представление имен файла
+        File file = new File("img/java.jpg");
+        // для загрузки изображения из файла применяется статический метод read() из класса ImageIO
+        BufferedImage image = ImageIO.read(file);
+        // для записи используется метод write
+        ImageIO.write(image, "png",  new File("java.png"));
 
-            // более высокое качество проприросвки
-            gr2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            Ellipse2D ellipse2D = new Ellipse2D.Double(50, 50, 100, 100);
-            gr2d.draw(ellipse2D);
 
-            // более низское качество прорисовки
-            RenderingHints renderingHints1 = new RenderingHints(null);
-            renderingHints1.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
-            gr2d.setRenderingHints(renderingHints1);
-            Ellipse2D ellipse2D1 = new Ellipse2D.Double(150, 50, 100,100);
-            gr2d.draw(ellipse2D1);
+        // # 2 Получение средств чтения и записи изображений по типам файлов
 
+        /*
+        Для выполнения расширенных операций записи и чтения изображений,которые выходят за пределы простого использования статических методов read() и write() из класса ImagelO,
+        нужно прежде всего получить объекты типа ImageReader и ImageWriter соответственно. В классе ImagelO перечисляются средства и записи, отвечающиео дному из следующих условий.
+
+        • Форматизображения,например JPEG.
+        • Расширениефайла,например jpg.
+        • ТипMIME,например image/jpeg.
+        */
+
+        //  расширения файлов для всех доступных средств чтения
+        String[] extensions = ImageIO.getWriterFileSuffixes();
+        for(String ext: extensions) {
+            System.out.printf(ext + " ");
         }
-    }
 
-    static JFrame getFrame() {
-        JFrame jframe = new JFrame();
-        jframe.setVisible(true);
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension dimension = toolkit.getScreenSize();
-        jframe.setBounds(dimension.width / 2 - 250, dimension.height / 2 - 150, 550, 300);
-        jframe.setTitle("Example");
-        return jframe;
-    }
+        // Класс ImagelO может обнаружить несколько средств чтения, каждое изкоторых способно обработать файлы данного типа
+        ImageReader reader = null;
+        Iterator<ImageReader> iterator = ImageIO.getImageReadersByFormatName("JPEG");
+        if(iterator.hasNext()) reader = iterator.next();
 
+        //  # 3 чтение и запись файлов с несколькими изображениями
+
+        // Для чтения нескольких изображений нужно преобразовать сначала источник входных данных (например, поток ввода или файл) в объект потока ввода изображений типа ImagelnputStream следующим образом:
+        InputStream inputSteam = new FileInputStream(new File("img/java.jpg"));
+        ImageInputStream imagein = ImageIO.createImageInputStream(inputSteam);
+        // Затем поток ввода следует присоединить к средству чтения, вызвав следующий метод:
+        // поток ввода находится в режиме просмотра данных только в прямом направлении
+        reader.setInput (imagein, true);
+        // Прочитать изображение непосредственно из файла
+        ImageInputStream imageIn = ImageIO.createImageInputStream(new File("img/java.jpg"));
+        // Вводим из этого потока изображение, вызывав метод read с передачей кол-ва изображений
+        // параметр true разраешает поиск и подсчет количесвтва изображений
+        BufferedImage image1 = reader.read(reader.getNumImages(true));
+
+
+        // # 4 Некоторые файлы могут содержать миниатюрные виды изображений
+
+        // Выясняем количество миниатюрных видов изображей,
+        int count = reader.getNumThumbnails(0);
+        // Извлечь конкретный миниатюрный вид по его индексу можно следующим образом
+        BufferedImage image2 = reader.readThumbnail(0, 0);
+        // Для получения размеров изображения по указанному индексу вызываются следующие методы:
+        System.out.println(reader.getHeight(0));
+        System.out.println(reader.getWidth(0));
+
+        // Для записи нескольких изображений в файл нужно создать сначала объект типа ImageWriter:
+        ImageWriter writer = null;
+        // Затем с помощью класса IOImage перечислить все средства,способные записывать изображения в конкретном формате:
+        Iterator<ImageWriter> iterator1 = ImageIO.getImageWritersByFormatName("JPEG");
+        if(iterator1.hasNext()) writer = iterator1.next();
+        // Затем поток ввода следует преобразовать в объект типа ImageOutputStream:
+        ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(new File("img/java.jpg"));
+        // и присоединить его к средству записи следующим образом:
+        writer.setOutput(imageOutputStream)
+
+    }
 }
