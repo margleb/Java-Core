fd508bdfbba82d6603989439a6b47d99f0704ae2
margleb <margleb@yandex.ru>
2020-03-07 16:32:59 +0300

    Урок по Java 134 AWT 14 Print PostScript

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 
 import javax.print.*;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, PrintException {
-       DocFlavor flavor = DocFlavor.INPUT_STREAM.JPEG;
-       PrintService[] services = PrintServiceLookup.lookupPrintServices(flavor, null);
-       InputStream inputStream = new FileInputStream("img/java.jpg");
-       Doc doc = new SimpleDoc(inputStream, flavor, null);
-       if(services.length > 0) {
-           DocPrintJob job = services[0].createPrintJob();
-           job.print(doc, null);
-       }
+        DocFlavor flavor = DocFlavor.SERVICE_FORMATTED.PAGEABLE;
+        String mineType = "application/postscript";
+        StreamPrintServiceFactory[] factories = StreamPrintServiceFactory.lookupStreamPrintServiceFactories(flavor, mineType);
+        OutputStream outputStream = new FileOutputStream("PostService.odd");
+        StreamPrintService streamPrintService = factories[0].getPrintService(outputStream);
+
+        InputStream inStream = new FileInputStream("img/java.jpg");
+        DocPrintJob job = streamPrintService.createPrintJob();
+        Doc doc = new SimpleDoc(inStream, DocFlavor.INPUT_STREAM.JPEG, null);
+        job.print(doc, null);
     }
 }
