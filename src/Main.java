d1759a9f21b4920ac4a23fd8b5ebbc15d604cd67
margleb <margleb@yandex.ru>
2020-03-08 12:20:45 +0300

    Урок по Java 138 AWT 18 Заставка - Splash screen

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,72 @@ import java.awt.*;
 import java.io.IOException;
 
 public class Main {
-    static JFrame jframe = addWindow();
-    static JPanel jPanel = new JPanel();
-    public static void main(String[] args) throws IOException {
-        jframe.add(jPanel);
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        JTextField jTextField = new JTextField(10);
-        JTextField jTextField1 = new JTextField(10);
+        // #1
+        /* Прописываем в VMOptions или командной строке -splash:img/flower.jpg */
+        // получаем ссылку на экран
+        SplashScreen splashScreen = SplashScreen.getSplashScreen();
+        // сведения о его графическом контексте
+        // Graphics2D g2 = splashScreen.createGraphics();
+        // размеры экрана
+        // Rectangle bounds = splashScreen.getBounds();
 
-        jTextField.setDragEnabled(true);
-        jTextField1.setDragEnabled(true);
+        // g2.setColor(Color.blue);
+        // for(int i = 0; i < 100; i++) {
+            // g2.fillRect(0,0, bounds.width * i, 20);
+            // вызываем метод update для гарантирования обновления рисунка
+            // splashScreen.update();
+            // Thread.sleep(100);
+        //}
 
-        jPanel.add(jTextField);
-        jPanel.add(jTextField1);
+        // #2
+        /* У рисования прямо на начальном экране имеется следующий существенный недостаток:
+        вычислять позиции всех пикселей утомительно, а рисуемый индикатор выполнения обычно не согласуется с собственным индикатором используемой платформы
+        Поэтомув самом начале выполнения метода main() лучше заменить начальный экран последующим окном с теми же размерами и содержимым */
 
-        jframe.revalidate();
-    }
+        // получаем копию изображения от splashScreen
+        final Image img = new ImageIcon(splashScreen.getImageURL()).getImage();
+        // Cоздаем фрейм
+        final JFrame splashFrame = new JFrame();
+        // прорисовываем панель на фрейме с изображением
+        final JPanel splashPanel = new JPanel() {
+            public void paintComponent(Graphics g) {
+                g.drawImage(img,0,0,null);
+            }
+        };
+        // создаем прогресс-бар
+        final JProgressBar progressBar = new JProgressBar();
+        // отображаем строку прогресса в индикаторе выполнения
+        progressBar.setStringPainted(true);
 
-    static public JFrame addWindow() {
-        JFrame jframe = new JFrame();
-        jframe.setVisible(true);
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Toolkit toolkit = Toolkit.getDefaultToolkit();
-        // Dimension dimension = toolkit.getScreenSize();
-        jframe.setBounds(0,0, 500,300);
-        jframe.setTitle("Окно");
-        return jframe;
+        // добавляем макет на панель BorderLayout();
+        splashPanel.setLayout(new BorderLayout());
+        // добавляем прогресс-бар с расположениемем NORTH
+        splashPanel.add(progressBar, BorderLayout.NORTH);
+        // добавляем панель на фрейм
+        splashFrame.add(splashPanel);
+        // устанавливаем размеры фрейма как размеры splashScreen
+        splashFrame.setBounds(splashScreen.getBounds());
+        // устанавливаем видимость на true
+        splashFrame.setVisible(true);
+
+        try {
+            // счетчик до 100
+            for(int i = 0; i <= 100; i++) {
+                // текст пргресс bar "Загрузка + %"
+                progressBar.setString("Loading module " + i);
+                // устанавливаем значение пргрузки пргресс бар
+                progressBar.setValue(i);
+                // перерисовывем панель
+                splashPanel.repaint();
+                // поток засыпает на 100 millis
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {}
+
+        // устанавливаем видимость на false
+        splashFrame.setVisible(false);
     }
 }
 
