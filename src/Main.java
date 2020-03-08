77778745f7ec2b4222588eb506723766e5ade89d
margleb <margleb@yandex.ru>
2020-03-08 11:20:17 +0300

    Урок по Java 137 AWT 17 DragAndDrop

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,34 @@
+import javax.swing.*;
 import java.awt.*;
-import java.awt.datatransfer.*;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, UnsupportedFlavorException
-    {
-        // системный буфер обмена
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        // создаем изображение
-        Image image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+    static JFrame jframe = addWindow();
+    static JPanel jPanel = new JPanel();
+    public static void main(String[] args) throws IOException {
+        jframe.add(jPanel);
 
-        // получаем графику изображения
-        Graphics g = image.getGraphics();
-        // устанавлиеваем цвет
-        g.setColor(Color.BLUE);
-        // рисуем прямоугольник
-        g.fillRect(0, 0, 100, 100);
-        // устанвливаем цвет
-        g.setColor(Color.RED);
-        // рисуем овал
-        g.fillOval(100 / 4, 100 / 4, 100 / 2, 100 / 2);
+        JTextField jTextField = new JTextField(10);
+        JTextField jTextField1 = new JTextField(10);
 
+        jTextField.setDragEnabled(true);
+        jTextField1.setDragEnabled(true);
 
-        // cоздаем экземпля класса ImageTransferable
-        ImageTransferable selection = new ImageTransferable(image);
-        // заносим его в буфер
-        clipboard.setContents(selection, null);
+        jPanel.add(jTextField);
+        jPanel.add(jTextField1);
 
-        // список доступных разноваидностей данных
-        // DataFlavor[] flavors = clipboard.getAvailableDataFlavors();
-        // for(DataFlavor df: flavors) {
-            // System.out.println(df);
-        // }
+        jframe.revalidate();
+    }
 
+    static public JFrame addWindow() {
+        JFrame jframe = new JFrame();
+        jframe.setVisible(true);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Toolkit toolkit = Toolkit.getDefaultToolkit();
+        // Dimension dimension = toolkit.getScreenSize();
+        jframe.setBounds(0,0, 500,300);
+        jframe.setTitle("Окно");
+        return jframe;
     }
 }
 
-// Объекты,которые требуется передать через буфер обмена,должны реализовывать интерфейс Transferable.
-class ImageTransferable implements Transferable {
-
-    private Image thelmage;
-
-    // конструктор с передачей изображения
-    public ImageTransferable(Image image) {
-        thelmage = image;
-    }
-
-    @Override
-    // Предоставляет мета информацию о данных
-    // mimetype=image/x-java-image;representationclass=java.awt.Image
-    public DataFlavor[] getTransferDataFlavors() {
-        // DataFlavor.imageFlavor - константа, выполняющая все необходимые действия по преобразованиию изображений
-        return new DataFlavor[]{DataFlavor.imageFlavor};
-    }
-
-    @Override
-    // Проверка на поддержку данных
-    public boolean isDataFlavorSupported(DataFlavor flavor) {
-        return flavor.equals(DataFlavor.imageFlavor);
-    }
-
-    @Override
-    // получает обьект
-    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException {
-        // если данные поддерживаются, вернуть изображение и наче ошибка
-        if (flavor.equals(DataFlavor.imageFlavor)) {
-            return thelmage;
-        } else {
-            throw new UnsupportedFlavorException(flavor);
-        }
-    }
-}
