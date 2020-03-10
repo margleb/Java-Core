1b197b7365c704d04bb768a23bd57f607755799b
margleb <margleb@yandex.ru>
2020-03-10 18:09:16 +0300

    Уроки по Java 149 Логирование - loggers

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,39 @@
-import example.Example;
+/* У нас есть логгеры, у которых могут быть несколько уровней, хендлеры, можем вывыодит в консоль, файл и т.д., использовать фильтры  */
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.prefs.BackingStoreException;
-import java.util.prefs.Preferences;
+import java.util.logging.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException, BackingStoreException {
-        // Поиск требуемого узла в дереве начинается с пользовательского или системного корня
-        // Preferences root = Preferences.systemRoot();
-        Preferences user = Preferences.userRoot();
-        // положить ключ значение
-        user.put("key", "value");
-        // при получении значения можно указать дефолтное значение
-        System.out.println(user.get("key", "default value"));
-        // для достуа к узлу достаточно указать соответствующий путь
-        Preferences node = user.node("myPackage");
-        // данные в хранилище можно записать используя методы
-        node.put("one", "1");
-        // получить доступ к узлу можжно обратившись к таблице с парами "ключ-значение"
-        System.out.println(node.getInt("one", 2));
-        // Для быстрого и удобного доступа к узлу дерева путь к нему приравнивается к имени пакета его класса
-        // Preferences systemPackage = Preferences.systemNodeForPackage(Example.class);
-        Preferences userPackage = Preferences.userNodeForPackage(Example.class);
-        // Данные можно экспортировать в поддерево, а в некторых случая в отдельны узел, вызвав один из приведенных ниже методов
-        user.exportSubtree(new FileOutputStream("myProperies.xml"));
+    // лог с указанием имени класса
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        // можно добавлять хендлеры (обработчики я так понял)
+        // для сохранения
+        Handler consoleHandler = new ConsoleHandler();
+        // cохранение в файл с указанием паттерна
+        // Handler fileHandler = new FileHandler("%h/myJavaLog.log");
+        // это фильтр
+        consoleHandler.setFilter(new MyFilter());
+        // а это форматирование
+        consoleHandler.setFormatter(new MyFormatter());
+        logger.addHandler(consoleHandler);
+        logger.setUseParentHandlers(false);
+        logger.info("info");
+        logger.info("info max");
+    }
+
+    static class MyFilter implements Filter {
+        @Override
+        public boolean isLoggable(LogRecord record) {
+            return record.getMessage().endsWith("max");
+        }
+    }
+
+    static class MyFormatter extends Formatter {
+        @Override
+        public String format(LogRecord record) {
+            return record.getLevel() + ": " + record.getMessage();
+        }
     }
 }
