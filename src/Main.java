08997c928155ebfc62d8c607247c7d52c2de5434
margleb <margleb@yandex.ru>
2020-03-11 17:40:34 +0300

    Урок Java 170 Классы Path и Files введенные в Java 7

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,89 @@
-/*
-// текстовые файлы
-Reader
-Writer
-// для байтов
-InputStream
-OutStream
- */
-
-import java.awt.image.BufferedImage;
 import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.zip.ZipInputStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // используются для текстовых файлов
-        // Reader reader;
-        // Writer writer;
-        // для работы с байтовыми файлами (потоковые стримы чтение из файла, сети и т.д)
-        // InputStream inputStream;
-        // OutputStream outputStream;
-
-        // Reader reader = new FileReader("temp.txt");
-        // int i = 0;
-        // while((i = reader.read()) != -1 ) {
-            // System.out.println((char)i);
-        // }
-        // char[] chars = new char[50];
-        // while(reader.read(chars) != -1) {
-            // for(int i = 0; i < chars.length; i++) {
-                // if(Character.isAlphabetic(chars[i]) || Character.isWhitespace(chars[i])) {
-                    // System.out.println(chars[i]);
-                // }
-            // }
-            // System.out.println(chars);
+        // Path path;
+        // Files files;
+        // File file = new File("some.file");
+        // if(file.exists()) {
+            // file.createNewFile();
         // }
 
-        //
-//        Writer writer = new FileWriter("temp.txt");
-//        // запись в память
-//        writer.write("str1\n");
-//        writer.write("str2\n");
-//        // запись в текстовый докумет
-//        writer.flush();
-//        writer.close(); // закрывает (автоматом вызывает flush)
-//
-//
-//        // читает в буфер, в отличии от прочтениея каждого символа из файла
-//        BufferedReader reader = new BufferedReader(new FileReader("temp.txt"));
-//        while(reader.ready()) {
-//            // читаем по строчно
-//            System.out.println(reader.readLine());
-//        }
-//
-//        // нет необходимости вызывать close
-//        try(BufferedWriter writer1 = new BufferedWriter(new FileWriter("temp.txt"))) {
-//            writer1.write("str1");
-//            writer1.newLine();
-//            writer1.write("str2");
-//        }
-//
-//
-//        // возможно читать только байты!! Buffered позволяет читать в буфер
-//        BufferedInputStream inputSteam = new BufferedInputStream(new FileInputStream("temp.txt"));
-//        int i = 0;
-//        while((i = inputSteam.read()) != -1) {
-//              System.out.println(i);
-//        }
-//        inputSteam.read();
-//
-//        // прочтение в массив (кол-во доступных байтов)
-//        byte[] bytes = new byte[inputSteam.available()];
-//        while(inputSteam.read(bytes) != -1) {
-//            for(int j = 0; j < bytes.length; i++) {
-//                // if(bytes[j] == 0) {
-//                    // break;
-//                // }
-//                System.out.println(bytes[j]);
-//            }
-//        }
+        // в абсолютный путь
+        Path path = Paths.get("temp.txt").toAbsolutePath();
+        File file = new File("temp.txt");
+        // path = file.toPath(); // Возвращает объект Path, созданный из * этого абстрактного пути.
 
-        // прочитывает байты, буферезированный быстрее считывает
-        try(BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream("temp.txt"))) {
-            byte[] bytes = new byte[] {49, 10, 50};
-            outputStream.write(bytes);
+        System.out.println(path.getFileName());
+        System.out.println(path.getParent());
+        System.out.println(path.isAbsolute());
+
+        // cкопировать
+        Files.copy(Paths.get("temp.txt"), Paths.get("temp2.txt"), StandardCopyOption.REPLACE_EXISTING);
+        // меняем имя либо перемещаем
+        Files.move(Paths.get("temp2.txt"), Paths.get("temp3.txt"));
+        // удлаем, если существует
+        Files.deleteIfExists(Paths.get("temp3.txt"));
+        // выводим размер файла
+        System.out.println(Files.size(path));
+        System.out.println(Files.isDirectory(path));
+        System.out.println(Files.isWritable(path));
+        System.out.println(Files.isReadable(path));
+        System.out.println(Files.getAttribute(path, "size", LinkOption.NOFOLLOW_LINKS));
+
+        // атрибуты
+        // BasicFileAttributes attributes = Files.readAttributes(path, BasicFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
+        // PosixFileAttributes posix = Files.readAttributes(path, PosixFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
+
+        // прочитываем все байты из файла
+        byte[] bytes = Files.readAllBytes(path);
+        for(int i = 0; i < bytes.length; i++) {
+            System.out.println(bytes[i]);
+        }
+        // Кодирует эту  строку в последовательность байтов, используя кодировку латформы по умолчанию, сохраняя результат в новом байтовом массиве.
+        Files.write(path, "bla".getBytes());
+
+        // прочитать все строки и вывести в список
+        java.util.List<String> list = Files.readAllLines(Paths.get("temp.txt"));
+        for(String s: list) {
+            // Читать построчно
+            System.out.println(s);
         }
 
+        java.util.List<String> list1 = new ArrayList<String>();
+        list1.add("new");
+        // можно записывать
+        Files.write(path, list1);
+        // Можно получать Input/Output stream и Reader/Writer
+        InputStream inputStream = Files.newInputStream(path);
+        OutputStream outputStream = Files.newOutputStream(path);
+        Reader reader = Files.newBufferedReader(path);
+        Writer writer  = Files.newBufferedWriter(path);
 
-        // можно создатвать цепочку стримов
-        ZipInputStream zipInputStream = new ZipInputStream(new DataInputStream(new BufferedInputStream(new FileInputStream(""))));
+        // Пробегает по списку дерикторий
+        try(DirectoryStream<Path> entries = Files.newDirectoryStream(Paths.get(".")))  {
+            for(Path path1: entries) {
+                System.out.println(path1.getFileName());
+            }
+        }
+
+        // пробегаемся по файлам и видоизменем их при необходимости
+        Files.walkFileTree(Paths.get("."), new HashSet<FileVisitOption>(), 1, new MyFileVisitor());
+
+    }
+}
+
+// экстендим SimpleFileVisitor (ecmь еще FileVisitor)
+class MyFileVisitor extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println(file.getFileName());
+        return FileVisitResult.CONTINUE;
     }
 }
