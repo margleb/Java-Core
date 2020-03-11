a9b31916b21b0a7d087ec0b51109b098c6af0698
margleb <margleb@yandex.ru>
2020-03-11 17:47:44 +0300

    Урок Java 171 Scanner и PrintWriter

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,17 @@
 import java.io.*;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.PosixFileAttributes;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.zip.ZipInputStream;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // Path path;
-        // Files files;
-        // File file = new File("some.file");
-        // if(file.exists()) {
-            // file.createNewFile();
-        // }
-
-        // в абсолютный путь
-        Path path = Paths.get("temp.txt").toAbsolutePath();
-        File file = new File("temp.txt");
-        // path = file.toPath(); // Возвращает объект Path, созданный из * этого абстрактного пути.
-
-        System.out.println(path.getFileName());
-        System.out.println(path.getParent());
-        System.out.println(path.isAbsolute());
-
-        // cкопировать
-        Files.copy(Paths.get("temp.txt"), Paths.get("temp2.txt"), StandardCopyOption.REPLACE_EXISTING);
-        // меняем имя либо перемещаем
-        Files.move(Paths.get("temp2.txt"), Paths.get("temp3.txt"));
-        // удлаем, если существует
-        Files.deleteIfExists(Paths.get("temp3.txt"));
-        // выводим размер файла
-        System.out.println(Files.size(path));
-        System.out.println(Files.isDirectory(path));
-        System.out.println(Files.isWritable(path));
-        System.out.println(Files.isReadable(path));
-        System.out.println(Files.getAttribute(path, "size", LinkOption.NOFOLLOW_LINKS));
-
-        // атрибуты
-        // BasicFileAttributes attributes = Files.readAttributes(path, BasicFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
-        // PosixFileAttributes posix = Files.readAttributes(path, PosixFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
-
-        // прочитываем все байты из файла
-        byte[] bytes = Files.readAllBytes(path);
-        for(int i = 0; i < bytes.length; i++) {
-            System.out.println(bytes[i]);
-        }
-        // Кодирует эту  строку в последовательность байтов, используя кодировку латформы по умолчанию, сохраняя результат в новом байтовом массиве.
-        Files.write(path, "bla".getBytes());
-
-        // прочитать все строки и вывести в список
-        java.util.List<String> list = Files.readAllLines(Paths.get("temp.txt"));
-        for(String s: list) {
-            // Читать построчно
-            System.out.println(s);
-        }
-
-        java.util.List<String> list1 = new ArrayList<String>();
-        list1.add("new");
-        // можно записывать
-        Files.write(path, list1);
-        // Можно получать Input/Output stream и Reader/Writer
-        InputStream inputStream = Files.newInputStream(path);
-        OutputStream outputStream = Files.newOutputStream(path);
-        Reader reader = Files.newBufferedReader(path);
-        Writer writer  = Files.newBufferedWriter(path);
-
-        // Пробегает по списку дерикторий
-        try(DirectoryStream<Path> entries = Files.newDirectoryStream(Paths.get(".")))  {
-            for(Path path1: entries) {
-                System.out.println(path1.getFileName());
+        try(Scanner scanner = new Scanner(new FileReader("temp.txt"))) {
+            while(scanner.hasNext()) {
+                System.out.println(scanner.next());
             }
-        }
-
-        // пробегаемся по файлам и видоизменем их при необходимости
-        Files.walkFileTree(Paths.get("."), new HashSet<FileVisitOption>(), 1, new MyFileVisitor());
-
-    }
-}
-
-// экстендим SimpleFileVisitor (ecmь еще FileVisitor)
-class MyFileVisitor extends SimpleFileVisitor<Path> {
-    @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.getFileName());
-        return FileVisitResult.CONTINUE;
+        };
+        try(PrintWriter writer = new PrintWriter(new FileWriter("temp.txt"))) {
+            writer.write("str1");
+            writer.write("\n");
+            writer.write("str1");
+        };
     }
 }
