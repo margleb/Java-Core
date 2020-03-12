10e51a95c51fce483cb4126133d3d8476b4b1cb5
margleb <margleb@yandex.ru>
2020-03-12 11:27:46 +0300

    Урок Java 176 XML5 XPath и работа с XML

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,30 @@
-import javax.xml.stream.XMLOutputFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
-        XMLOutputFactory factory  = XMLOutputFactory.newFactory();
-        XMLStreamWriter writer = factory.createXMLStreamWriter(new FileOutputStream("temp2.xml"));
-        writer.writeStartDocument();
-        writer.writeStartElement("root");
-        writer.writeStartElement("font");
-        writer.writeAttribute("size", "20");
-        writer.writeCharacters("TimesNewRoman");
-        writer.writeEndElement();
-        writer.writeEndElement();
+    public static void main(String[] args) throws IOException, XMLStreamException, ParserConfigurationException, SAXException, XPathExpressionException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File(".idea/modules.xml"));
+
+        // обьект типа XPath для вычеслениея элементов
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xpath = xPathFactory.newXPath();
+        // /gridbag/row[1]/cell[1] @Sanchor
+        // /gridbag/row[1]
+        // count(/project/component/output)
+        // System.out.println(xpath.evaluate("/project/component/output", document));
+        // NodeList list = xpath.evaluate("/project/component/output", document, XPathConstants.NODESET);
     }
 }
