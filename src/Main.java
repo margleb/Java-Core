7303e629af63832a69c72daff91da1cf77f656c8
margleb <margleb@yandex.ru>
2020-03-12 11:03:01 +0300

    Урок Java 175 XML4 Создание XML, SAX подход

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,19 @@
-import org.w3c.dom.DOMImplementation;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Text;
-import org.w3c.dom.ls.DOMImplementationLS;
-import org.w3c.dom.ls.LSOutput;
-import org.w3c.dom.ls.LSSerializer;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, TransformerException, IOException {
-
-        // cоздаем документ
-        DocumentBuilderFactory  factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.newDocument();
-        Element root =  document.createElement("root");
-        Element font = document.createElement("font");
-        Text text = document.createTextNode("TimesNewRoman");
-        document.appendChild(root);
-        root.appendChild(font);
-        font.appendChild(text);
-        font.setAttribute("size", "20");
-
-        // сохраняем
-        Transformer t = TransformerFactory.newInstance().newTransformer();
-        t.setOutputProperty(OutputKeys.INDENT, "yes");
-        t.transform(new DOMSource(document), new StreamResult(new FileOutputStream("temp.xml")));
-
-        DOMImplementation impl = document.getImplementation();
-        DOMImplementationLS implLS = (DOMImplementationLS)impl.getFeature("LS", "3.0");
-        LSSerializer ser = implLS.createLSSerializer();
-        ser.getDomConfig().setParameter("format-pretty-print", true);
-        // сохрание в строчку
-        String str = ser.writeToString(document);
-        System.out.println(str);
-
-        // cохраняем в файл
-        LSOutput out = implLS.createLSOutput();
-        out.setEncoding("UTF-8");
-        out.setByteStream(Files.newOutputStream(Paths.get("temp.xml")));
-        ser.write(document, out);
-
+    public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
+        XMLOutputFactory factory  = XMLOutputFactory.newFactory();
+        XMLStreamWriter writer = factory.createXMLStreamWriter(new FileOutputStream("temp2.xml"));
+        writer.writeStartDocument();
+        writer.writeStartElement("root");
+        writer.writeStartElement("font");
+        writer.writeAttribute("size", "20");
+        writer.writeCharacters("TimesNewRoman");
+        writer.writeEndElement();
+        writer.writeEndElement();
     }
 }
