79a7c0536a33bc00886c1e58a1e8ba7ed469b896
margleb <margleb@yandex.ru>
2020-03-12 10:36:18 +0300

    Урок Java Xml2 Sax подход

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,52 @@
-import org.w3c.dom.*;
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import org.xml.sax.helpers.DefaultHandler;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        // DOM анализатор
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        // прочитываем документ по указаному адресу, получаем древовидную структур XML-документа
-        Document document  = builder.parse(new File(".idea/workspace.xml"));
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, XMLStreamException {
 
-        // возращает корневой элемент
-        Element element = document.getDocumentElement();
-        // извлекаем дочерние элементы по отношению к данному
-        NodeList nodeList = element.getChildNodes();
-        // метод getLength() - общее количество элеменетов
-        for(int i = 0; i < nodeList.getLength(); i++) {
-            // Метод item() возвращает элемент набора данных по указанному индексу
-            Node child = nodeList.item(i);
-            // игнарирование всех разделителей
-            if (child instanceof Element) {
-                // получтить nег
-                // System.out.println(((Element) nodeList.item(i)).getTagName());
-            }
-        };
-        printElement(nodeList, 0);
-    }
-    // выводим все теги
-    static void printElement(NodeList nodeList, int tabs) {
-        for(int i = 0; i < nodeList.getLength(); i++) {
-            if (nodeList.item(i) instanceof Element) {
-
-                String value = "";
-                // если не пустой текст, если первый элемент с не пустой информацией, и если это не переход на новую строку
-                if(!nodeList.item(i).getTextContent().trim().isEmpty() &&
-                !((Text)nodeList.item(i).getFirstChild()).getData().trim().isEmpty()
-                && !((Text)nodeList.item(i).getFirstChild()).getData().trim().equals("\n")) {
-                    // берем текст
-                    Text text = (Text)nodeList.item(i).getFirstChild();
-                    value += " = " + text.getData().trim();
-                }
-                System.out.println(getTab(tabs) + ((Element) nodeList.item(i)).getTagName());
-                // выводим узел если у него есть атрибут name
-                if((((Element) nodeList.item(i)).hasAttribute("name"))) {
-                    System.out.println(((Element) nodeList.item(i)).getAttribute("name"));
-                }
-
-                // проверка на наличие дочернего элемента
-                if(nodeList.item(i).hasChildNodes()) {
-                    // рекрусивный вызов функции
-                    printElement(nodeList.item(i).getChildNodes(), ++tabs);
+        DefaultHandler handler = new DefaultHandler() {
+            @Override
+            public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                String name = attributes.getValue("name");
+                if(name != null && !name.isEmpty()) {
+                    System.out.println(name);
                 }
             }
+            @Override
+            public void characters (char ch[], int start, int length) {
+                String str = "";
+                for(int i = 0; i < length; i++) {
+                    str += ch[start + i];
+                }
+                System.out.println(str);
+            }
         };
-    }
-    // табуляция
-    static String getTab(int tabs) {
-        String str = "";
-        for(int i = 0; i < tabs; i++) {
-            str += "\t";
+
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = factory.newSAXParser();
+        parser.parse(new File(".idea/misc.xml"), handler);
+
+
+        // Новый Sax подход
+        XMLInputFactory factory2 = XMLInputFactory.newInstance();
+        XMLStreamReader parser2 = factory2.createXMLStreamReader(new FileInputStream(".idea/misc.xml"));
+        while(parser2.hasNext()) {
+            int event = parser2.next();
+            if(event == XMLStreamConstants.START_ELEMENT) {
+                System.out.println(parser2.getLocalName());
+            }
         }
-        return str;
+
     }
 }
